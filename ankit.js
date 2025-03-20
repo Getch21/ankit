9e8e9958605f5961e9122d721d8339d0fc4de601
getch <getch.talk21@gmail.com>
2025-03-20 23:21:12 +0530

    first commit by ankit

---
+++ ankit.js
@@ -0,0 +1,9 @@
+// alert('do not open this page you are hacked');
+// var a = prompt('do not open this page you are hacked');
+var a = 188798787;
+var b = 47545544;
+var ans = a+b;
+
+window.onload = function() {
+    document.getElementById("innerdiv").innerHTML = ans;
+};
